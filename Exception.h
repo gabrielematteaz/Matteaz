6838fe120688b64719a5f67febf51adbab92f3b9
Matteaz <79192782+gabrielematteaz@users.noreply.github.com>
2024-01-27 11:33:04 +0100

    Added explanatory comments

--- Exception.h
+++ Exception.h
@@ -6,17 +6,22 @@ namespace Matteaz
 {
 	class Exception
 	{
-		HANDLE heap;
 		wchar_t* message;
+		HANDLE heap;
 
-	protected:
-		Exception(HANDLE heap, const wchar_t* message) noexcept :
-			heap(heap),
-			message(NULL)
+	public:
+		/* Since this constructor is designed not to throw any axceptions and the copying
+		process of "message" might fail, "this->message" is not guaranteed to be
+		a valid copy (It might be NULL) */
+		explicit Exception(const wchar_t* message = NULL, HANDLE heap = NULL) noexcept :
+			message(NULL),
+			heap(heap)
 		{
-			if (heap != NULL && message != NULL)
+			if (message != NULL && heap != NULL)
 			{
-				size_t messageLength = wcslen(message) + 1; /* Maybe use "wcsnlen" (No need for "wcsnlen_s") */
+				/* "wcsnlen" is safer but I can't come up with a meaningful maximum
+				length (There is no need to use "wcsnlen_s" since I already checked if "message" is NULL) */
+				size_t messageLength = wcslen(message) + 1;
 
 				wchar_t* messageCopy = static_cast < wchar_t* > (HeapAlloc(heap, 0, messageLength * sizeof(wchar_t)));
 				if (messageCopy != NULL) wcsncpy_s(messageCopy, messageLength, message, messageLength);
@@ -25,22 +30,15 @@ namespace Matteaz
 			}
 		}
 
-	public:
-		explicit Exception(const wchar_t* message = NULL) noexcept :
-			Exception(GetProcessHeap(), message)
-		{
-
-		}
-
 		Exception(const Exception& exception) noexcept :
-			Exception(exception.heap, exception.message)
+			Exception(exception.message, exception.heap)
 		{
 
 		}
 
 		constexpr Exception(Exception&& exception) noexcept :
-			heap(exception.heap),
-			message(exception.message)
+			message(exception.message),
+			heap(exception.heap)
 		{
 			exception.message = NULL;
 		}
@@ -55,19 +53,27 @@ namespace Matteaz
 		{
 			if (message != exception.message)
 			{
-				wchar_t* messageCopy = exception.message;
+				wchar_t* messageCopy = NULL;
 
-				if (exception.heap != NULL && exception.message != NULL)
+				if (exception.message != NULL && exception.heap != NULL)
 				{
-					size_t messageLength = wcslen(exception.message) + 1; /* Maybe use "wcsnlen" (No need for "wcsnlen_s") */
-				
+					/* Go to the constructor definition to know why I
+					used "wcslen" instead of "wcsnlen" */
+					size_t messageLength = wcslen(exception.message) + 1;
+
 					messageCopy = static_cast < wchar_t* > (HeapAlloc(exception.heap, 0, messageLength * sizeof(wchar_t)));
 					if (messageCopy != NULL) wcsncpy_s(messageCopy, messageLength, exception.message, messageLength);
 				}
-				
-				HeapFree(heap, 0, message);
-				heap = exception.heap;
-				message = messageCopy;
+
+				/* If the next line is commented then you commit to the fact
+				that you could lose the previous message in case the copying process fails */
+				if (messageCopy != NULL)
+				{
+					HeapFree(heap, 0, message);
+					message = messageCopy;
+
+					heap = exception.heap;
+				}
 			}
 
 			return *this;
@@ -78,15 +84,25 @@ namespace Matteaz
 			if (message != exception.message)
 			{
 				HeapFree(heap, 0, message);
-				heap = exception.heap;
 				message = exception.message;
+
+				heap = exception.heap;
+
 				exception.message = NULL;
 			}
 
 			return *this;
 		}
 
-		[[nodiscard]] virtual const wchar_t* Message() const noexcept
+		[[nodiscard]] constexpr HANDLE Heap() const noexcept
+		{
+			return heap;
+		}
+
+		/* This class is not designed to let "message" be modified but if you really wanted to do
+		so just cast away the const-ness of the pointer returned by this
+		function (It is safe since the original message was copied in the constructor) */
+		[[nodiscard]] constexpr virtual const wchar_t* Message() const noexcept
 		{
 			return message;
 		}
