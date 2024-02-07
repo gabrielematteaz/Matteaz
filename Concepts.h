a3cb78ed35434c59e4add534ce4d4c30eac941f1
Matteaz <79192782+gabrielematteaz@users.noreply.github.com>
2024-02-07 13:07:01 +0100

    unused

--- Concepts.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include < type_traits >
-
-namespace Matteaz
-{
-	template < typename Type >
-	concept NoExtentsDestructible = std::is_nothrow_destructible_v < std::remove_all_extents_t < Type > >;
-}
