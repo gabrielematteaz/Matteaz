c9c4ad6eef593a1335832ca0bd90d1d1eb9a34a5
Matteaz <79192782+gabrielematteaz@users.noreply.github.com>
2024-07-25 11:38:10 +0200

    fixed

--- concurrency.cc
+++ concurrency.cc
@@ -2,6 +2,8 @@
 
 namespace matteaz
 {
+	struct _shared shared;
+
 	void mutex::lock() noexcept
 	{
 		AcquireSRWLockExclusive(&lock_);
@@ -16,5 +18,43 @@ namespace matteaz
 	bool mutex::try_lock() noexcept
 	{
 		return TryAcquireSRWLockExclusive(&lock_);
+	}
+
+	void mutex::lock(const _shared &) noexcept
+	{
+		AcquireSRWLockShared(&lock_);
+	}
+
+	void mutex::unlock(const _shared &) noexcept
+	{
+#pragma warning(suppress : 26110)
+		ReleaseSRWLockShared(&lock_);
+	}
+
+	bool mutex::try_lock(const _shared &) noexcept
+	{
+		return TryAcquireSRWLockShared(&lock_);
+	}
+
+	exclusive_lock::exclusive_lock(mutex &mutex) noexcept :
+		mutex_(mutex)
+	{
+		mutex.lock();
+	}
+
+	exclusive_lock::~exclusive_lock()
+	{
+		mutex_.unlock();
+	}
+
+	shared_lock::shared_lock(mutex &mutex) noexcept :
+		mutex_(mutex)
+	{
+		mutex.lock(shared);
+	}
+
+	shared_lock::~shared_lock()
+	{
+		mutex_.unlock(shared);
 	}
 }
--- concurrency.h
+++ concurrency.h
@@ -4,21 +4,54 @@
 
 namespace matteaz
 {
+	struct _shared { };
+
+	extern _shared shared;
+
 	class mutex
 	{
 		SRWLOCK lock_;
 
 	public:
-		mutex(const mutex&) = delete;
-		mutex &operator = (const mutex&) = delete;
+		mutex(const mutex &) = delete;
+		mutex &operator = (const mutex &) = delete;
 		void lock() noexcept;
 		void unlock() noexcept;
 		[[nodiscard]] bool try_lock() noexcept;
+		void lock(const _shared &) noexcept;
+		void unlock(const _shared &) noexcept;
+		[[nodiscard]] bool try_lock(const _shared &) noexcept;
 
 		constexpr mutex() noexcept :
 			lock_(SRWLOCK_INIT)
 		{
 
 		}
+	};
+
+	class exclusive_lock
+	{
+		mutex &mutex_;
+
+	public:
+		using mutex_type = mutex;
+
+		exclusive_lock(mutex &mutex) noexcept;
+		exclusive_lock(const exclusive_lock &) = delete;
+		~exclusive_lock();
+		exclusive_lock &operator = (const exclusive_lock &) = delete;
+	};
+
+	class shared_lock
+	{
+		mutex &mutex_;
+
+	public:
+		using mutex_type = mutex;
+
+		shared_lock(mutex &mutex) noexcept;
+		shared_lock(const shared_lock &) = delete;
+		~shared_lock();
+		shared_lock &operator = (const shared_lock &) = delete;
 	};
 }
